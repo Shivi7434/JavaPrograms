d9494562e42868330ad75849ca85e999dd6a9ea4
Shivi7434 <shivalikhanuja.net@gmail.com>
2018-03-07 16:28:12 -0600

    first commit

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Tim!");
+
+        int myFirstNumber = 20*5 +60;
+        System.out.println(myFirstNumber);
+        System.out.println(myFirstNumber);
+    }
+}
