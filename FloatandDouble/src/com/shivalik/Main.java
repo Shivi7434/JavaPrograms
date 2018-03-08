1614fd5d0334d6feab86b25d8519d458c79f6e23
Shivi7434 <shivalikhanuja.net@gmail.com>
2018-03-08 00:00:40 -0600

    floatanddouble

---
+++ FloatandDouble/src/com/shivalik/Main.java
@@ -0,0 +1,21 @@
+package com.shivalik;
+
+public class Main {
+
+    public static void main(String[] args) {
+    //width of int 32
+	//int myIntValue = 5 / 2;
+	//width of float = 32
+	//float myFloatValue = 5f /3f;
+	// width of double = 64
+	//double myDoubleValue = 5d /3d; //double precision is faster on may computer
+	//System.out.println("myIntValue = " + myIntValue);
+	//System.out.println("myFloatValue = " + myFloatValue);
+	//System.out.println("myDoubleValue = " + myDoubleValue);
+
+        double numPounds = 200d;
+        double convertedKg    = numPounds * 0.45359237d;
+
+        System.out.println("Kilograms " + convertedKg);
+     }
+}
