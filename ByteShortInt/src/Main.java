ce7fc05d5a56923c46ccba38c19176eed2f8bdd0
Shivi7434 <shivalikhanuja.net@gmail.com>
2018-03-07 23:10:42 -0600

    byteintshort

---
+++ ByteShortInt/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    //datatype based on whole number
+    public static void main(String[] args) {
+        // has a width of 32
+        int myMinValue = -2_147_483_648; //int is used when more space is available
+        int myMaxValue = 2_147_483_647;
+        // has a width of 8
+        byte myByteValue = -128; //allocated for much smaller amount of space largest number that can fit is 127
+        // has a width of 16
+        short myshortValue = -32768; // largest no. can be stored is 32767
+
+
+        //long has a width of 64
+        long myLongValue = 9_223_372_036_854_775_807L; // can store a huge number
+     }
+}
--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -3,7 +3,14 @@ public class Hello {
         System.out.println("Hello Tim!");
 
         int myFirstNumber = 20*5 +60;
+        int mySecondNumber = 10;
+        int myThirdNumber = 12;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastOne = 2000 - myTotal;
+
         System.out.println(myFirstNumber);
-        System.out.println(myFirstNumber);
+        System.out.println(myTotal);
+        System.out.println(myLastOne);
     }
 }
