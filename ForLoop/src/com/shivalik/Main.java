7ad3b123b1860916a785102f752f452752365eec
Shivi7434 <shivalikhanuja.net@gmail.com>
2018-03-13 00:52:01 -0500

    more switch and for loop

--- ForLoop/src/com/shivalik/Main.java
+++ ForLoop/src/com/shivalik/Main.java
@@ -7,7 +7,17 @@ public class Main {
         System.out.println("10,000 at 3% interest = " + calculateInterest(10000.0, 3.0));
         System.out.println("10,000 at 4% interest = " + calculateInterest(10000.0, 4.0));
         System.out.println("10,000 at 5% interest = " + calculateInterest(10000.0, 5.0));
+
+        //for(init; termination; increment){ //when to intiate; when to exit the code; keep going until teminate condition met
+
+        for(int i = 0; i < 5; i++){
+            System.out.println("Loop " + i + " hello");
+        }
+        for ( int i = 2; i < 9 ; i++){
+            System.out.println("Interest amount is " + calculateInterest(10000.0, i ));
+        }
     }
+
     public static double calculateInterest(double amount, double interestRate) {
         return(amount * (interestRate/100));
     }
