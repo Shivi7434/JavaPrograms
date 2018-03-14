3cc6f8dbf696e2cb075acf9280e72fd571e80d38
Shivi7434 <shivalikhanuja.net@gmail.com>
2018-03-14 00:40:39 -0500

    while loop

--- While/src/com/shivalik/Main.java
+++ While/src/com/shivalik/Main.java
@@ -3,8 +3,21 @@ package com.shivalik;
 public class Main {
 
     public static void main(String[] args) {
-        int count =0;
+        int count =1;
         while(count !=5){
+            System.out.println("Count value is " + count);
+            count++;
+        }
+        //for the for loop which give same results
+        //for ( count = 1; count < 6; count++) {
+            //System.out.println("Count value is " + count);
+       // }
+
+        count = 1;
+        while(true) {
+            if(count == 6){
+                break;
+            }
             System.out.println("Count value is " + count);
             count++;
         }
