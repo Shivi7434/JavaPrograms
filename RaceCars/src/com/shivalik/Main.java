50dc4cbbaa8b96359bfa91d6c01823c638f64845
Shivi7434 <shivalikhanuja.net@gmail.com>
2018-03-15 20:48:20 -0500

    classes

--- RaceCars/src/com/shivalik/Main.java
+++ RaceCars/src/com/shivalik/Main.java
@@ -4,35 +4,57 @@ public class Main {
 
 
     public static void main(String[] args) {
-        boolean game = true;
-        int speed = 100;
-        int distance = 25;
-        int laps = 6;
+//        boolean game = true;
+//        int speed = 100;
+//        int distance = 25;
+//        int laps = 6;
+//
+//        if (speed >= 100)
+//            System.out.println("Speed less than " + speed + " for " + distance + " miles");
+//        if (speed >= 500)
+//            System.out.println("Speed less than " + speed);
+//
+//        if (game) {
+//            int totalDistance = distance * laps;
+//            System.out.println("Distance Covered " + totalDistance + " miles");
+//
+//        }
+//
+//        String name = "Tim";
+//        switch(name) {
+//            case "Tim":
+//                System.out.println("Tim" );
+//                break;
+//
+//            case "Carrie":
+//                System.out.println("Carrie");
+//                break;
+//
+//            default:
+//                System.out.println("Someone Else  "); //to find how to use string in switch
+//                break;
+//        }
+//    }
 
-        if (speed >= 100)
-            System.out.println("Speed less than " + speed + " for " + distance + " miles");
-        if (speed >= 500)
-            System.out.println("Speed less than " + speed);
+        int number = 2;
+        int finishNumber = 50;
 
-        if (game) {
-            int totalDistance = distance * laps;
-            System.out.println("Distance Covered " + totalDistance + " miles");
+        while( number <= finishNumber ){
+            number++;
+            if (!isEvenNumber(number)) {
+                continue;
+            }
+            System.out.println(" Even number is " + number);
 
         }
 
-        String name = "Tim";
-        switch(name) {
-            case "Tim":
-                System.out.println("Tim" );
-                break;
+    }
 
-            case "Carrie":
-                System.out.println("Carrie");
-                break;
-
-            default:
-                System.out.println("Someone Else  "); //to find how to use string in switch
-                break;
+    public static boolean isEvenNumber(int number){
+        if ((number % 2) == 0){
+            return true;
+        }else {
+            return false;
         }
     }
 }
--- While/src/com/shivalik/Main.java
+++ While/src/com/shivalik/Main.java
@@ -3,23 +3,71 @@ package com.shivalik;
 public class Main {
 
     public static void main(String[] args) {
-        int count =1;
-        while(count !=5){
-            System.out.println("Count value is " + count);
-            count++;
-        }
-        //for the for loop which give same results
-        //for ( count = 1; count < 6; count++) {
-            //System.out.println("Count value is " + count);
-       // }
+//        int count =1;
+////        while(count !=5){
+////            System.out.println("Count value is " + count);
+////            count++;
+////        }
+//        //for the for loop which give same results
+//        //for ( count = 1; count < 6; count++) {
+//            //System.out.println("Count value is " + count);
+//       // }
+//
+//        count = 1;
+////        while(true) {
+////            if(count == 6){
+////                break;
+////            }
+////            System.out.println("Count value is " + count);
+////            count++;
+////        }
+//
+//        do {
+//            System.out.println("Count value was " + count);
+//            count++;
+//
+//            if(count > 100){
+//                break;
+//            }
+//        }while (count != 6);
 
-        count = 1;
-        while(true) {
-            if(count == 6){
+//        int number = 4;
+//        int finishNumber = 20;
+//
+//        while (number <= finishNumber ){
+//            number++;
+//            if(!isEvenNumber(number)){
+//                continue;
+//            }
+//            System.out.println("Even number " + number);
+//        }
+
+        int number = 4;
+        int finishNumber = 20;
+        int evenNumberFound = 0;
+
+        while (number <= finishNumber ) {
+            number++;
+            if (!isEvenNumber(number)) {
+                continue;
+            }
+            System.out.println("Even number " + number);
+            evenNumberFound++;
+            if (evenNumberFound >= 5) {
                 break;
             }
-            System.out.println("Count value is " + count);
-            count++;
         }
+            System.out.println("Total even number found " + evenNumberFound);
+
+
+    }
+
+    public static boolean isEvenNumber (int number){
+        if ((number % 2) == 0){
+            return true;
+        }else {
+            return false;
+        }
+
     }
 }
